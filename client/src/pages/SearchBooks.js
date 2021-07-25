55e02af84de0803e33f99450cdb31c00866436dc
Kevin Crawford <crawfordkevin101@gmail.com>
2021-07-24 20:54:14 -0400

    added schemas folder with resolvers, typeDefs and index.js

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 import { SAVE_BOOK } from '../utils/mutations';
 import {useMutation} from '@apollo/react-hooks';
@@ -137,6 +137,7 @@ const SearchBooks = () => {
                     </Button>
                   )}
                 </Card.Body>
+                {error && <div> Something went wrong...</div>}
               </Card>
             );
           })}
